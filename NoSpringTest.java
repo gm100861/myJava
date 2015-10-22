c4775ca037c074eb70c04ca4ef6b3c855e664f43
gm100861 <gm100861@gmail.com>
2015-10-22 15:17:54 +0800

    Create NoSpringTest.java

---
+++ NoSpringTest.java
@@ -0,0 +1,39 @@
+package com.plusnet.search.core.dal.ibatis;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+public class NoSpringTest {
+
+    public static void main(String[] args) {
+        //System.out.println(converterToSpell("虎(音si)亭镇"));
+        String abc = "abc ";
+        System.out.println(abc);
+        abc = abc.trim();
+        System.out.println(abc);
+    }
+
+    public static String converterToSpell(String chines) {
+        StringBuffer sb = new StringBuffer();
+        char[] nameChar = chines.toCharArray();
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (int i = 0; i < nameChar.length; i++) {
+            if (nameChar[i] > 128) {
+                try {
+                    sb.append(PinyinHelper.toHanyuPinyinStringArray(
+                            nameChar[i], defaultFormat)[0]);
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            } else {
+                sb.append(nameChar[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
