cb5c0cbf94e37ff15fb382b6a973b632381de7a2
gm100861 <gm100861@gmail.com>
2015-10-29 23:32:25 +0800

    Create ChildrenChannelHandler.java

---
+++ nioNetty/ChildrenChannelHandler.java
@@ -0,0 +1,18 @@
+package org.linuxsogood.netty.b;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by m on 15/10/29.
+ */
+public class ChildrenChannelHandler extends ChannelInitializer<SocketChannel>{
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        socketChannel.pipeline().addLast(new StringDecoder());
+        socketChannel.pipeline().addLast(new TimeServerHandel());
+    }
+}
