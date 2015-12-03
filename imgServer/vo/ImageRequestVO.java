4dcb3fbf5c3bc165b80b558d7a0ba5652ff434fe
gm100861 <gm100861@gmail.com>
2015-12-03 11:32:25 +0800

    Create ImageRequestVO.java

---
+++ imgServer/vo/ImageRequestVO.java
@@ -0,0 +1,17 @@
+package com.plusnet.search.img.vo;
+
+import java.util.Map;
+
+public class ImageRequestVO {
+
+    private Map<String,String> urls;
+
+    public Map<String, String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(Map<String, String> urls) {
+        this.urls = urls;
+    }
+    
+}
